5b72810aba327fa946a05c05ea56533111b912d4
Juan <juantrelles@gmail.com>
2020-02-20 23:15:09 -0600

    stereoscopic cameras for 180 degrees angular fisheye projection, and samples from Stitcher

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public List<SnapshotCamera> snapCams;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            foreach (SnapshotCamera snapCam in snapCams) {
+                snapCam.CallTakeSnapshot();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/SnapshotCamera.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class SnapshotCamera : MonoBehaviour
+{
+    Camera snapCam;
+
+    int resWidth = 256;
+    int resHeight = 256;
+
+
+    void Awake()
+    {
+        snapCam = GetComponent<Camera>();
+        // backup if not rendered texture is assigned to camera
+        if (snapCam.targetTexture == null)
+        {
+            snapCam.targetTexture = new RenderTexture(resWidth, resHeight, 24);
+        }
+        else {
+            resWidth = snapCam.targetTexture.width;
+            resHeight = snapCam.targetTexture.height;
+        }
+
+        snapCam.gameObject.SetActive(false);
+    }
+
+    public void CallTakeSnapshot() {
+        snapCam.gameObject.SetActive(true);
+    }
+
+    void LateUpdate()
+    {
+        
+        if (snapCam.gameObject.activeInHierarchy) {
+            Texture2D snapshot = new Texture2D(resWidth, resHeight, TextureFormat.RGB24, false);
+            snapCam.Render();
+            RenderTexture.active = snapCam.targetTexture;
+            snapshot.ReadPixels(new Rect(0, 0, resWidth, resHeight), 0, 0);
+            byte[] bytes = snapshot.EncodeToPNG();
+            string fileName = SnapshotName();
+            System.IO.File.WriteAllBytes(fileName, bytes);
+            // Debug.Log("Snapshot Taken");
+            snapCam.gameObject.SetActive(false);
+        }
+    }
+
+    string SnapshotName() {
+        return string.Format("{0}/Snapshots/snap_{1}_{2}.png", 
+            Application.dataPath,             
+            System.DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss"),
+            this.gameObject.name);
+    }
+}
